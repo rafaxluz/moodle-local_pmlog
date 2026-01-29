aa3f7ac2ca8220c5de9af4e02870dbaeed6da89c
rafaxluz <107878482+rafaxluz@users.noreply.github.com>
2026-01-29 09:56:45 -0300

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_pmlog';
+$plugin->version   = 2026011202;
+$plugin->requires  = 2019111800;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1.0';
