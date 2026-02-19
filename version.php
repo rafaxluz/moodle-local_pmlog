2328d0fb07e976e67c0eb075c413e4dc65fc15f5
rafaxluz <107878482+rafaxluz@users.noreply.github.com>
2026-02-19 13:45:28 -0300

    Change plugin version in version.php
    Updated plugin version number to 2026021901.

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_pmlog';
-$plugin->version   = 2026190201;
+$plugin->version   = 2026021901;
 $plugin->requires  = 2019111800;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.5';
+
